8b2010128336652886f69638d70d6790e6e9196c
Mitra-Electronics <ishanmitra020@gmail.com>
2024-12-24 20:43:58 +0530

    Surveys route added

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 import src.accounts.router as arouter
+import src.survey.router as srouter
 
 app = FastAPI()
 app.include_router(arouter.app, prefix="/accounts")
+app.include_router(srouter.app, prefix="/surveys")
 
 
 @app.get("/registe")
