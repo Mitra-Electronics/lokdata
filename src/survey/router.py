662f4ba3148677ed44a27de94bfff0de0234d7b0
Mitra-Electronics <ishanmitra020@gmail.com>
2024-12-24 22:18:08 +0530

    Get Survey URL added

--- src/survey/router.py
+++ src/survey/router.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter
 
 app = APIRouter()
 
+@app.post("/get")
+def get_survey():
+    return {"success":"dummy"}
+
 @app.post("/create")
 def create_survey():
     return {"success":"dummy"}
