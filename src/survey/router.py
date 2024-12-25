0d920e18bc6544ce72f0bbd0e7056c284c5e1760
Mitra-Electronics <ishanmitra020@gmail.com>
2024-12-25 13:06:21 +0530

    Create schema added

--- src/survey/router.py
+++ src/survey/router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from src.survey.schemas import SurveyCreate
 
 app = APIRouter()
 
@@ -7,5 +8,5 @@ def get_survey():
     return {"success":"dummy"}
 
 @app.post("/create")
-def create_survey():
+def create_survey(input: SurveyCreate):
     return {"success":"dummy"}
---
+++ src/survey/schemas.py
@@ -0,0 +1,5 @@
+from pydantic import BaseModel
+
+class SurveyCreate(BaseModel):
+    creator_id: str
+    title: str
